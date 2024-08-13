8e7356c5abb555fd515be18751879eeab8c23fde
Johan van Rhyn <johan@squarehole.dev>
2024-08-13 19:38:30 +0200

    Fixed a type in go.mod file

---
+++ cmd/sun/models.go
@@ -0,0 +1,35 @@
+package sun
+
+type (
+	Weather struct {
+		Location struct {
+			Name    string `json:"name"`
+			Country string `json:"country"`
+		} `json:"location"`
+		Current struct {
+			LastUpdatedEpoch int64   `json:"last_updated_epoch"`
+			Temperature      float64 `json:"temp_c"`
+			Condition        struct {
+				Text string `json:"text"`
+			} `json:"condition"`
+			WindSpeed    float64 `json:"wind_kph"`
+			Gusts        float64 `json:"gust_kph"`
+			ChanceOfRain int64   `json:"chance_of_rain"`
+		} `json:"current"`
+		Forecast struct {
+			ForecastDay []struct {
+				DateEpoch int64 `json:"date_epoch"`
+				Hours     []struct {
+					DateEpoch   int64   `json:"time_epoch"`
+					Temperature float64 `json:"temp_c"`
+					Condition   struct {
+						Text string `json:"text"`
+					} `json:"condition"`
+					WindSpeed    float64 `json:"wind_kph"`
+					Gusts        float64 `json:"gust_kph"`
+					ChanceOfRain int64   `json:"chance_of_rain"`
+				} `json:"hour"`
+			} `json:"forecastday"`
+		} `json:"forecast"`
+	}
+)
